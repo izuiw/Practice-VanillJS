28ec476ebef95f9c2ac5c0abcf5aa70e6c740705
izuiw <aswll.kr@gmail.com>
2024-01-15 22:55:18 +0900

    2024.01.15_ todoApp(삭제/완료 버튼 이벤트 처리)

--- TodoApp/js/todo.js
+++ TodoApp/js/todo.js
@@ -2,7 +2,33 @@
 const todoInput = document.querySelector("#todo-input");
 const todoList = document.querySelector(".todo-list");
 
-const likeButton = document.querySelector(".like");
+const allclear = document.querySelector(".all-clear");
+const alldelete = document.querySelector(".all-delete");
+
+
+//전체 삭제 - 화살표function
+
+alldelete.addEventListener("click", () => {
+    if(todoList.firstChild){
+        while(todoList.firstChild){
+            todoList.removeChild(todoList.firstChild);
+        }
+    }
+
+      
+})
+
+//전체 완료 - 화살표function
+allclear.addEventListener("click", () =>{
+    if(todoList.firstChild){
+        console.log(todoList.firstChild)
+        while(todoList.firstChild){
+             todoList.firstChild.classList.add('done') 
+             //2024.01.15 Error 
+                //todo.js:26 Uncaught TypeError: Cannot read properties of undefined (reading 'add') -> 객체를 제대로 찾지 못함.. while 범위 탓?
+        }
+    }
+})
 
 
 //input 이벤트
@@ -11,7 +37,7 @@ todoInput.addEventListener("keypress", function(e){
         console.log(todoInput.value); 
         
         //li
-        generateTodo(todoInput.value);ß
+        generateTodo(todoInput.value);
         todoInput.value = "";
         
     }
@@ -31,6 +57,7 @@ function generateTodo(todo){
     todoList.appendChild(li);
 };
 
+//const likeButton = document.querySelector(".like");
 function generateLike() {
     const span = document.createElement("span");
     span.classList.add("todo-like");
@@ -41,6 +68,13 @@ function generateLike() {
     icon.innerText = "favorite_border";
     span.appendChild(icon);
 
+    //like 버튼 이벤트 
+    span.addEventListener("click",()=>{
+        //삼항 연산자 
+            //조건 ? 참 : 거짓
+        icon.innerHTML === 'favorite_border' ? icon.innerHTML = "favorite" : icon.innerHTML = "favorite_border"
+        
+    })
     return span;
 };
 
@@ -65,6 +99,19 @@ function generateManage(){
     icon2.classList.add("clear");
     icon2.innerText = "clear";
 
+    //완료, 삭제 이벤트
+    icon1.addEventListener("click", (e)=>{
+        const li = e.target.parentNode.parentNode;
+        li.classList.add('done')
+    })
+
+    icon2.addEventListener("click", (e)=>{
+        const li = e.target.parentNode.parentNode;
+        todoList.removeChild(li)
+    })
+
+    
+
     span.appendChild(icon1);
     span.appendChild(icon2);
     
@@ -72,4 +119,3 @@ function generateManage(){
     return span;
 };
 
-//like 버튼 이벤트 
