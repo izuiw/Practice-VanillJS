8bebef62d1f683f6e6dc4ab82fe416c496a438a5
izuiw <aswll.kr@gmail.com>
2025-08-22 00:58:14 +0900

    feat: login#2

--- MomentumApp/src/app.js
+++ MomentumApp/src/app.js
@@ -1,1 +1,16 @@
 const loginInput = document.querySelector("#login-form input");
+const loginForm = document.querySelector("#login-form");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const userName = loginInput.value;
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+
+  //localStorage에 user저장
+  localStorage.setItem("userName", userName);
+
+  greeting.innerHTML = `Hello ${userName}!`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+});
